1d35ad9b01bfad6648178ccb5d9d5f946031dd67
bajdcc <bajdcc@foxmail.com>
2017-05-05 11:17:06 +0800

    Fix bug

--- src/kernel/proc.c
+++ src/kernel/proc.c
@@ -211,6 +211,7 @@ void wakeup(uint8_t pid) {
     for (pp = pcblist; pp <= &pcblist[NPROC]; pp++) {
         if (pp->pid == pid && pp->state == P_SLEEPING) {
             pp->state = P_RUNABLE;
+            return;
         }
     }
 
@@ -280,7 +281,7 @@ void exit() {
         return;
     }
     
-    for (pp = pcblist; pp < &proc[NPROC]; pp++) {
+    for (pp = pcblist; pp < &pcblist[NPROC]; pp++) {
         if (pp->parent == proc) { // 找到子进程
             pp->parent = proc->parent; // 将子进程的父进程置为当前进程的父进程（当前进程skip）
             if (pp->state == P_ZOMBIE) { // 若子进程为僵尸进程
